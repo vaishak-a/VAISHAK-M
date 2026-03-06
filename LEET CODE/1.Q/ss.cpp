67cbd8f6a6df19e56f4ab723a61737398d8ae564
VAISHAK M <vaishakvaishak007@gmail.com>
2026-03-06 11:16:05 +0530

    Create ss.cpp

---
+++ LEET CODE/1.Q/ss.cpp
@@ -0,0 +1,10 @@
+bool checkOnesSegment(string s)
+{
+    for (int i = 1; i < s.length(); i++) {
+       
+        if (s[i] == '1' && s[i - 1] == '0') {
+            return false;
+        }
+    }
+    return true;
+}
