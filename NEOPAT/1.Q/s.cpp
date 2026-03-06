aaaf132bbb62791d5fd638b70ad6b76a7c642d5a
VAISHAK M <vaishakvaishak007@gmail.com>
2026-03-06 11:25:40 +0530

    Create s.cpp

---
+++ NEOPAT/1.Q/s.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name[10];
+    cin>>name;
+    char pass[10];
+    cin>>pass;
+    cout<<"Name: "<<name<<endl;
+    cout<<"Password: "<<pass;
+    return 0;
+}
