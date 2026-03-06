6364e4215564e426d9648b26c190413e35151559
VAISHAK M <vaishakvaishak007@gmail.com>
2026-03-06 11:38:32 +0530

    Update s.cpp

--- NEOPAT/8.Q/s.cpp
+++ NEOPAT/8.Q/s.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+using  namespace std;
+int main()
+{
+    int a;
+    char p;
+    cin>>a;
+    if(a<1||a>9)
+    {
+        cout<<"Invalid Input";
+    }
+    else
+    {
+        cin>>p;
+        if(p=='y')
+        {
+            cout<<a+5;
+        }
+        else
+        {
+            cout<<a;
+        }
+    }
+    return 0;
+}
 
